57f370dbb100933d67f63da2c83cefd0539cafe1
chetkiy1988 <chetkiy1988@gmail.com>
2024-03-08 00:59:06 +0200

    add google sheets api

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
+import {google} from "googleapis"
+import {Server} from "socket.io"
 import dotenv from "dotenv"
 import OpenAI from "openai"
 import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
-dotenv.config()
-import {Server} from "socket.io"
 import http from "http"
-import { env } from "process"
+dotenv.config()
+
 
 const app = express()
 app.use(cors())
@@ -24,6 +25,59 @@ const io = new Server(server, {
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
+
+
+
+const serviceAccountKeyFile = "./djura-chat-09231493e075.json";
+const sheetId = '1hb_7oXfhETxvLiOMdp5lESKp4XrwM-KvAw-FMBH4Ldc'
+const tabName = 'users'
+const range = 'A:H'
+
+
+main().then(() => {
+    console.log('Completed')
+  })
+  
+  async function main() {
+    // Generating google sheet client
+    const googleSheetClient = await _getGoogleSheetClient();
+  
+    // Reading Google Sheet from a specific range
+    const data = await _readGoogleSheet(googleSheetClient, sheetId, tabName, range);
+    console.log(data);
+    
+    // Adding a new row to Google Sheet
+    const dataToBeInserted = [
+       ['11', 'rohith', 'Rohith', 'Sharma', 'Active'],
+       ['12', 'virat', 'Virat', 'Kohli', 'Active']
+    ]
+    // await _writeGoogleSheet(googleSheetClient, sheetId, tabName, range, dataToBeInserted);
+}
+
+
+async function _readGoogleSheet(googleSheetClient, sheetId, tabName, range) {
+    const res = await googleSheetClient.spreadsheets.values.get({
+        spreadsheetId: sheetId,
+        range: `${tabName}!${range}`,
+    });
+
+    return res.data.values;
+}
+
+
+async function _getGoogleSheetClient() {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: serviceAccountKeyFile,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const authClient = await auth.getClient();
+    return google.sheets({
+      version: 'v4',
+      auth: authClient,
+    });
+}
+
+
 
 
 app.use(function (req, res, next) {
